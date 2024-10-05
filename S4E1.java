66d6f11310e7be9da2597094d2a7a8e26e22f271
Erik Pham <erilopha@gmail.com>
2024-10-04 21:52:26 -0500

    committed set 4

--- S4E1.java
+++ S4E1.java
@@ -6,20 +6,20 @@ public class S4E1 {
         Random rand = new Random();
         Scanner scanner = new Scanner(System.in);
 
-        // Generate two random integers between 1 and 100
-        int num1 = rand.nextInt(100) + 1;
+        // Generate two random integers
+        int num1 = rand.nextInt(50) + 1;
         int num2 = rand.nextInt(100) + 1;
 
         // Prompt the user to enter the sum of the two integers
-        System.out.println("What is the sum of " + num1 + " and " + num2 + "?");
+        System.out.println("What is " + num1 + " + " + num2 + "?");
         int userAnswer = scanner.nextInt();
 
         // Check if the user's answer is correct
         if (userAnswer == num1 + num2) {
-            System.out.println("Correct! " + num1 + "+" + num2 + " is equal to " + userAnswer + "!");
+            System.out.println("Correct! " + num1 + " + " + num2 + " is equal to " + userAnswer + "!");
             System.out.println("Good job!");
         } else {
-            System.out.println("Oh no! " + (num1 + num2) + " is not equal to " + (userAnswer) + "!");
+            System.out.println("Oh no! " + num1 + " + " + num2 + " is not equal to " + (userAnswer) + "!");
             System.out.println("Keep practicing!");
         }
 
